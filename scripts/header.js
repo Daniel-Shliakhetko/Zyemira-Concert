80f3b93483044639b8aade13adda8e41cf04ff1f
Daniel-Shliakhetko <danielshliakhetko@gmail.com>
2022-08-06 13:48:06 +0300

    Added Header mobile adaptation

--- scripts/header.js
+++ scripts/header.js
@@ -1,7 +1,43 @@
-const paddingTop = $(".header").css("padding-top");
+const checkOnScroll = () => {
+  if (!($(window).width() < 768)) {
+    $(".header").css({
+      height: 200 - $(window).scrollTop() / 5,
+    });
+    $(window).scrollTop() > 200
+      ? $(".navigation__background").css({ opacity: 1 })
+      : $(".navigation__background").css({ opacity: 0 });
+  } else {
+    $(".header").css({
+      height: "fit-content",
+    });
+    $(".navigation__background").css({ opacity: 1 });
+  }
+};
 
-$(window).scroll(() => {
-  $(".header").css({
-    paddingTop: paddingTop - $(window).scrollTop() / 5,
-  });
-});
+const onChangeSize = () => {
+  if ($(window).width() < 768) {
+    $(".navigation").addClass("mobile");
+    $(".navigation__toggler").removeClass("active");
+    $(".navigation__list").hide("slow");
+  } else {
+    $(".navigation").removeClass("mobile");
+    $(".navigation__toggler").removeClass("active");
+    $(".navigation__list").show("slow");
+  }
+};
+
+const onTogglerClick = () => {
+  $(".navigation__toggler").toggleClass("active");
+  $(".navigation__list").toggle("slow");
+};
+
+onChangeSize();
+$(window).resize(onChangeSize);
+
+checkOnScroll();
+$(window).scroll(checkOnScroll);
+
+if ($(window).width() < 768) {
+
+}
+$(".navigation__toggler").click(onTogglerClick);
